5681589e47875c8ba3238be94170f4e9729cf595
brjapon <brjapon@therobotacademy.com>
2018-01-26 07:26:25 +0100

    '06_Ultrasonic_Distance_Sensor.ino': show measurements every second to have a stable reading

--- 06_Ultrasonic_Distance_Sensor/06_Ultrasonic_Distance_Sensor.ino
+++ 06_Ultrasonic_Distance_Sensor/06_Ultrasonic_Distance_Sensor.ino
@@ -11,5 +11,5 @@ void loop()
 {
   Serial.print("distance(cm) = ");          // Print the results to the serial monitor
   Serial.println(ultrasonic.distanceCm());  // Distance value from 3cm - 400cm
-  delay(50);                                // Wait 50 milliseconds before next measurement
+  delay(1000);                                // Wait 50 milliseconds before next measurement
 }
